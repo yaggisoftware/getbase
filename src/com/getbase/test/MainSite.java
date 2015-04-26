ba6a5f715f95ff7fc8238afe7d5723732d3648fb
yaggi <yaggi@poczta.fm>
2015-04-26 22:58:15 +0200

    Added JUnit support

--- src/com/getbase/test/MainSite.java
+++ src/com/getbase/test/MainSite.java
@@ -1,16 +1,23 @@
 package com.getbase.test;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MainSite {
 
-	public static void main(String[] args) {
+	@Test
+	public void firstTest() {
 		// Create a new instance of the Firefox driver
 		WebDriver driver = new FirefoxDriver();
 
 		// Open Base site
 		driver.get("https://getbase.com/");
+
+		Assert.assertEquals(
+				"Intelligent Sales Productivity Platform | Base CRM",
+				driver.getTitle());
 
 		// Close the browser
 		driver.quit();
